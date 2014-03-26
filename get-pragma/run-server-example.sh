e2e70bfaaf14b79a76831ddd9adc659cd5d0c1c3
Yuri Gil Dantas <ygd@ygd.debian>
2014-03-26 09:22:52 -0300

    Creating script for running based on architecture

--- get-pragma/run-server-example.sh
+++ get-pragma/run-server-example.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-export PATH=../maude/:$PATH
+MAUDE=../maude64
+ARCH=$(uname -m)
+
+case ${ARCH} in
+
+	i*86)
+
+		MAUDE=../maude
+		;;
+esac
+	
+export PATH=${MAUDE}:$PATH
 java -jar ../pvesta/pvesta-server.jar 1117
