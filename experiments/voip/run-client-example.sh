0694c794e0b6b1ea3fe658d78d25f71b17ba57a2
ygdantas@github.com <ygd@ci.ufpb.br>
2015-11-24 16:24:48 +0100

    New version of VoIP Formalization

--- experiments/voip/run-client-example.sh
+++ experiments/voip/run-client-example.sh
@@ -1,16 +1,39 @@
 #!/bin/sh
 #export PATH=../maude/:$PATH
 
+# --------------- Coordinated Attack with SeVen --------------#
+
 #java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_model_voip.maude -f client.quatex
 #java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_model_voip.maude -f attacker.quatex
 #java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_model_voip.maude -f availability.quatex
 #java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_model_voip.maude -f availabilityINC.quatex
 #java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_model_voip.maude -f ddos.quatex
-#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m sem_defesa_voip_flooding.maude -f success.quatex
-#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m sem_defesa_voip_flooding.maude -f success2.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_model_voip.maude -f tts.quatex
 
-java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f client.quatex
-java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f attacker.quatex
-java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f availability.quatex
-java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f availabilityINC.quatex
-java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f ddos.quatex
+# --------------- 911 Attack with SeVen --------------#
+
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f client.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f attacker.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f availability.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f availabilityINC.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f ddos.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m seven_911_model_voip.maude -f tts.quatex
+
+# --------------- Coordinated Attack without SeVen --------------#
+
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_coordattack_model_voip.maude -f client.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_coordattack_model_voip.maude -f attacker.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_coordattack_model_voip.maude -f availability.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_coordattack_model_voip.maude -f availabilityINC.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_coordattack_model_voip.maude -f ddos.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_coordattack_model_voip.maude -f tts.quatex
+
+# --------------- 911 Attack without SeVen --------------#
+
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_911_model_voip.maude -f client.quatex
+java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_911_model_voip.maude -f attacker.quatex
+java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_911_model_voip.maude -f availability.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_911_model_voip.maude -f availabilityINC.quatex
+java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_911_model_voip.maude -f ddos.quatex
+#java -jar ../../pvesta/pvesta-client.jar -l ../../pvesta/serverlist -m no_defense_911_model_voip.maude -f tts.quatex
+
